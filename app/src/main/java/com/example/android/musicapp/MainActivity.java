aba2fde99e8f0298b5ddeb6deaf89afb48d012d0
Tarun <trikha.tarun01@gmail.com>
2016-09-16 02:24:15 +0530

    Changed padding and added play pause button on click functionality

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.android.musicapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    int play = 0; //initially song is paused
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button playPause = (Button) findViewById(R.id.playPause);
+        playPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (play == 0){
+                    playPause.setBackgroundResource(R.drawable.pause);
+                    play = 1;
+                }
+                else {
+                    playPause.setBackgroundResource(R.drawable.play);
+                    play=0;
+                }
+            }
+        });
     }
 }
