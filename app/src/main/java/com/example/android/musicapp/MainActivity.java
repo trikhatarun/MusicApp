ecd78aaaae7174fb98e66d482c71502a168383fd
Tarun <trikha.tarun01@gmail.com>
2016-09-18 15:27:25 +0530

    added

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.musicapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -13,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         final Button playPause = (Button) findViewById(R.id.playPause);
         playPause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +43,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.buySongs) {
+            Intent currentSong = new Intent(MainActivity.this, currentSong.class);
+            startActivity(currentSong);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     public void playSong(View v){
         Intent currentSong = new Intent(MainActivity.this, currentSong.class);
         startActivity(currentSong);
